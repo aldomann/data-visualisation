e011b53ff105a3f59ede70255ba28d35924dd5e2
Alfredo Hernández <aldomann.designs@gmail.com>
2017-12-12 13:50:30 +0100

    First BN delivery

---
+++ bayesian-networks/intro.R
@@ -0,0 +1,11 @@
+---
+title: "Data Visualization"
+author: "Alfredo Hernández"
+subtitle: "Bayesian Networks with R"
+date: "11 December 2017"
+output:
+  html_notebook:
+    highlight: pygments
+    theme: cosmo
+    toc: yes
+---
